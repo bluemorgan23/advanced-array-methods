21f8d808df2201888b0902d5078c4a25cfcbd2fb
Chris Morgan <bluemorgan2323@gmail.com>
2019-03-19 15:04:58 -0500

    Completed the lightning exercise for filter method. Added two buttons at the top when the pageloads so you can choose which list to display.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,66 @@
 const outEl = document.querySelector("#output");
-outEl.innerHTML = "<h1>Active Businesses</h1>";
+const allButton = document.createElement("button");
+const manuButton = document.createElement("button");
+const header = document.querySelector("#header");
+allButton.textContent = "All Businesses";
+manuButton.textContent = "Manufacturing Businesses";
+header.appendChild(allButton);
+header.appendChild(manuButton);
 
-businesses.forEach(business => {
-    let zipCode = business.addressZipCode;
-    outEl.innerHTML += `
-    <h2>${business.companyName}</h2>
-    <section>
-      ${business.addressFullStreet}
-    </section>
-    <section>
-      ${business.addressCity}, ${business["addressStateCode"]}, ${zipCode}
-    </section>
-  `;
-    outEl.innerHTML += "<hr/>";
-})
 
+const getAllBiz = () => {
+    outEl.innerHTML = "<h1>Active Businesses</h1>";
+    businesses.forEach(business => {
+        let zipCode = business.addressZipCode;
+        outEl.innerHTML += `
+        <h2>${business.companyName}</h2>
+        <section>
+        ${business.addressFullStreet}
+        </section>
+        <section>
+        ${business.addressCity}, ${business["addressStateCode"]}, ${zipCode}
+        </section>
+        `;
+        outEl.innerHTML += "<hr/>";
+    })
+}
 // Array to contain all the New York businesses
 const newYorkBusinesses = businesses.filter(business => {
     let inNewYork = false;
-  
+
     if (business.addressStateCode === "NY") {
         inNewYork = true;
     }
-  
+
     return inNewYork;
 })
 
 const manufacturingBusinesses = businesses.filter(business => {
     let isManufacturing = false;
 
-    if(business.companyIndustry === "Manufacturing"){
+    if (business.companyIndustry === "Manufacturing") {
         isManufacturing = true;
+
     }
     return isManufacturing;
 })
 
+const getManufacturingBiz = () => {
+    outEl.innerHTML = "<h1>Active Businesses</h1>";
+    manufacturingBusinesses.forEach(business => {
+
+        outEl.innerHTML += `
+        <h2>${business.companyName}</h2>
+        <section>
+          ${business.addressFullStreet}
+        </section>
+        <section>
+            ${business.addressCity}, ${business.addressStateCode}, ${business.addressZipCode}
+        </section>`;
+        outEl.innerHTML += "<hr/>"
+    })
+
+}
+
+allButton.addEventListener("click", getAllBiz);
+manuButton.addEventListener("click", getManufacturingBiz);
